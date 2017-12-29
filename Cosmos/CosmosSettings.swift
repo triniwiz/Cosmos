bf4b5ba2023c4870c5569d17dcafa084cb5e10df
Osei Fortune <fortune.osei@yahoo.com>
2017-12-29 11:53:31 -0400

    ObjC exposure

--- Cosmos/CosmosSettings.swift
+++ Cosmos/CosmosSettings.swift
@@ -1,95 +1,100 @@
 import UIKit
 
-/**
 
-Settings that define the appearance of the star rating views.
+@objc public class CosmosSettings:NSObject {
+    override init() {}
 
-*/
-public struct CosmosSettings {
-  init() {}
-  
-  // MARK: - Star settings
-  // -----------------------------
-    
-  /// Border color of an empty star.
-  public var emptyBorderColor = CosmosDefaultSettings.emptyBorderColor
-  
-  /// Width of the border for empty star.
-  public var emptyBorderWidth: Double = CosmosDefaultSettings.emptyBorderWidth
-  
-  /// Border color of a filled star.
-  public var filledBorderColor = CosmosDefaultSettings.filledBorderColor
-  
-  /// Width of the border for a filled star.
-  public var filledBorderWidth: Double = CosmosDefaultSettings.filledBorderWidth
+    // MARK: - Star settings
+    // -----------------------------
 
-  /// Background color of an empty star.
-  public var emptyColor = CosmosDefaultSettings.emptyColor
-  
-  /// Background color of a filled star.
-  public var filledColor = CosmosDefaultSettings.filledColor
-  
-  /**
-  
-  Defines how the star is filled when the rating value is not a whole integer. It can either show full stars, half stars or stars partially filled according to the rating value.
-  
-  */
-  public var fillMode = CosmosDefaultSettings.fillMode
-  
-  /// Distance between stars.
-  public var starMargin: Double = CosmosDefaultSettings.starMargin
-  
-  /**
-  
-  Array of points for drawing the star with size of 100 by 100 pixels. Supply your points if you need to draw a different shape.
-  
-  */
-  public var starPoints: [CGPoint] = CosmosDefaultSettings.starPoints
-  
-  /// Size of a single star.
-  public var starSize: Double = CosmosDefaultSettings.starSize
-  
-  /// The maximum number of stars to be shown.
-  public var totalStars = CosmosDefaultSettings.totalStars
-  
-  // MARK: - Star image settings
-  // -----------------------------
-  
-  /**
-  
-  Image used for the filled portion of the star. By default the star is drawn from the array of points unless an image is supplied.
-  
-  */
-  public var filledImage: UIImage? = nil
-  
-  /**
-   
-   Image used for the empty portion of the star. By default the star is drawn from the array of points unless an image is supplied.
-   
-   */
-  public var emptyImage: UIImage? = nil
-  
-  // MARK: - Text settings
-  // -----------------------------
-  
-  /// Color of the text.
-  public var textColor = CosmosDefaultSettings.textColor
-  
-  /// Font for the text.
-  public var textFont = CosmosDefaultSettings.textFont
-  
-  /// Distance between the text and the stars.
-  public var textMargin: Double = CosmosDefaultSettings.textMargin
-  
-  
-  // MARK: - Touch settings
-  // -----------------------------
-  
-  /// The lowest rating that user can set by touching the stars.
-  public var minTouchRating: Double = CosmosDefaultSettings.minTouchRating
-  
-  /// When `true` the star fill level is updated when user touches the cosmos view. When `false` the Cosmos view only shows the rating and does not act as the input control.
-  public var updateOnTouch = CosmosDefaultSettings.updateOnTouch
+    /// Border color of an empty star.
+    @objc public var emptyBorderColor = CosmosDefaultSettings.emptyBorderColor
+
+    /// Width of the border for empty star.
+    @objc public var emptyBorderWidth: Double = CosmosDefaultSettings.emptyBorderWidth
+
+    /// Border color of a filled star.
+    @objc public var filledBorderColor = CosmosDefaultSettings.filledBorderColor
+
+    /// Width of the border for a filled star.
+    @objc public var filledBorderWidth: Double = CosmosDefaultSettings.filledBorderWidth
+
+    /// Background color of an empty star.
+    @objc public var emptyColor = CosmosDefaultSettings.emptyColor
+
+    /// Background color of a filled star.
+    @objc public var filledColor = CosmosDefaultSettings.filledColor
+
+    /**
+
+     Defines how the star is filled when the rating value is not a whole integer. It can either show full stars, half stars or stars partially filled according to the rating value.
+
+     */
+    var _fillMode = CosmosDefaultSettings.fillMode
+    @objc public var fillMode: StarFillMode{
+        get{
+            return _fillMode;
+        }
+        set(fillMode){
+            _fillMode = fillMode;
+        }
+    }
+
+    /// Distance between stars.
+    @objc public var starMargin: Double = CosmosDefaultSettings.starMargin
+
+    /**
+
+     Array of points for drawing the star with size of 100 by 100 pixels. Supply your points if you need to draw a different shape.
+
+     */
+    @objc public var starPoints: [CGPoint] = CosmosDefaultSettings.starPoints
+
+    /// Size of a single star.
+    @objc public var starSize: Double = CosmosDefaultSettings.starSize
+
+    /// The maximum number of stars to be shown.
+    @objc public var totalStars = CosmosDefaultSettings.totalStars
+
+    // MARK: - Star image settings
+    // -----------------------------
+
+    /**
+
+     Image used for the filled portion of the star. By default the star is drawn from the array of points unless an image is supplied.
+
+     */
+    @objc public var filledImage: UIImage? = nil
+
+    /**
+
+     Image used for the empty portion of the star. By default the star is drawn from the array of points unless an image is supplied.
+
+     */
+    @objc public var emptyImage: UIImage? = nil
+
+    // MARK: - Text settings
+    // -----------------------------
+
+    /// Color of the text.
+    @objc public var textColor = CosmosDefaultSettings.textColor
+
+    /// Font for the text.
+    @objc public var textFont = CosmosDefaultSettings.textFont
+
+    /// Distance between the text and the stars.
+    @objc public var textMargin: Double = CosmosDefaultSettings.textMargin
+
+
+    // MARK: - Touch settings
+    // -----------------------------
+
+    /// The lowest rating that user can set by touching the stars.
+    @objc public var minTouchRating: Double = CosmosDefaultSettings.minTouchRating
+
+    /// When `true` the star fill level is updated when user touches the cosmos view. When `false` the Cosmos view only shows the rating and does not act as the input control.
+    @objc public var updateOnTouch = CosmosDefaultSettings.updateOnTouch
+
+
 }
-
 
--- Cosmos/StarFillMode.swift
+++ Cosmos/StarFillMode.swift
@@ -5,13 +5,35 @@ import Foundation
 Defines how the star is filled when the rating is not an integer number. For example, if rating is 4.6 and the fill more is Half, the star will appear to be half filled.
 
 */
-public enum StarFillMode: Int {
+@objc public enum StarFillMode: Int, RawRepresentable {
   /// Show only fully filled stars. For example, fourth star will be empty for 3.2.
-  case full = 0
-  
+  case full
+
   /// Show fully filled and half-filled stars. For example, fourth star will be half filled for 3.6.
-  case half = 1
-  
+  case half
+
   /// Fill star according to decimal rating. For example, fourth star will be 20% filled for 3.2.
-  case precise = 2
+  case precise
+
+  public typealias RawValue = Int
+  public var rawValue: RawValue{
+        switch self {
+        case .half:
+            return 1;
+        case .precise:
+            return 2;
+        default:
+            return 0;
+        }
+  }
+  public init?(rawValue: RawValue){
+        switch rawValue {
+        case 1:
+            self = .half;
+        case 2:
+            self = .precise;
+        default:
+            self = .full;
+        }
+  }
 }
